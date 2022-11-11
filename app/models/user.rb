aba9b3c59971de6a45dda6a976171e87d6ef1aff
DeboDevelop <debajyotidofficial@gmail.com>
2022-11-11 19:47:07 +0530

    Added Intent model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :chat_bots
+  has_many :intents
 end
---
+++ db/migrate/20221111141042_create_intents.rb
@@ -0,0 +1,10 @@
+class CreateIntents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :intents do |t|
+      t.text :intent_name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
