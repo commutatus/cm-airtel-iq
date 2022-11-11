b088463e6824ecc593e35df69dfda29523b17c16
shrudhinvraj <shrudhinvraj1998@gmail.com>
2022-11-11 17:51:03 +0530

    cm-airtel :hammer: refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post 'webhooks/process'
+  post "/webhooks/process/:webhook_source", controller: :webhooks, action: :process
 end
