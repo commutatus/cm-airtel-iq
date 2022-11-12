7293d23f0ef12a347c3df4438c6875c025ba333b
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-12 13:00:31 +0530

    Root page with coming soon image (#4)
    * Chore: Add support for scss and slim
    
    * Feat: Landing page action
    
    * Chore: Asset for coming soon
    
    * Chore: .DS_Store banished!
    
    * Feat: Root page with coming soon image

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Controller action for the root/home page.
+class HomeController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  post "/webhooks/process/:webhook_source", controller: :webhooks, action: :process
+  root 'home#index'
+
+  post '/webhooks/process/:webhook_source', controller: :webhooks, action: :process
 end
