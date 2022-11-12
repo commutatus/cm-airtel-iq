968f77d1e4691599a8f1ab94de6e7ff47906c326
Aditya Tiwari <taditya.tiwari007@gmail.com>
2022-11-12 10:15:32 +0530

    Feat: Add the default URL option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.default_url_options = { host: 'airtel-hackathon.commutatus.com', protocol: 'https' }
 end
